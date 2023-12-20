fff93e75f42f436aa89b79ee4872ed0041c951c7
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-20 14:48:58 +0330

    creating view templates and url

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Movie import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movies/', views.movie_list, name='movie_list'),
 ]
